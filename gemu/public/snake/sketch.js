c1221c0b7f018062371b3a530da2865cd9b3d3db
Yong Dong <yongdong11467@gmail.com>
2019-04-29 17:01:36 -0400

    add imgs

--- gemu/public/snake/sketch.js
+++ gemu/public/snake/sketch.js
@@ -9,15 +9,45 @@ function setup() {
     ball = new Ball();
     food = createVector(random(width), random(height))
     socket = io.connect("http://localhost:3000/snake/")
+    socket.on("snake", anotherSnake);
+}
+
+function anotherSnake(data) {
+    snake.show();
+    snake.update();
 }
 
 function draw() {
+
     background(0);
     snake.show();
     snake.update();
 
     ball.show();
     ball.update();
+
+    //Seinding data
+    //console.log(snake.x + " " + snake.y);
+    var snakeData = {
+        snakex: snake.x,
+        snakey: snake.y
+    }
+
+    // var ballData = {
+    //     ballx: ball.x,
+    //     bally: ball.y
+    // }
+
+    // var foodData = {
+    //     foodx: food.x,
+    //     foody: food.y
+    // }
+
+    socket.emit('snake', snakeData);
+    console.log(snakeData);
+    // socket.emit("ball", ballData);
+    // socket.emit("food", foodData);
+    //end sending
 
     fill(240, 10, 100);
     rect(food.x, food.y, 10, 10)
--- gemu/server.js
+++ gemu/server.js
@@ -5,7 +5,7 @@ var server = app.listen(3000);
 
 app.use(express.static("public"));
 
-console.log("Connecting...");
+console.log("Waiting for client to connect...");
 
 var socket = require("socket.io");
 
@@ -13,5 +13,24 @@ var io = socket(server);
 io.sockets.on("connection", newConnection);
 
 function newConnection(socket) {
-    console.log(socket);
+    console.log("New client: " + socket.id);
+
+    socket.on('snake', snakeMsg);
+
+    function snakeMsg(data) {
+        socket.broadcast.emit('snake', data);
+        console.log(data);
+    }
+    
+    // socket.on("ball", ballMsg);
+
+    // function ballMsg(data) {
+    //     console.log(data);
+    // }
+
+    // socket.on("food", foodMsg);
+
+    // function foodMsg(data) {
+    //     console.log(data);
+    // }
 }
